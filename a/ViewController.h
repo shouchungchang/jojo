81296f7bc0bcd614afc7e34128b16c8eee82a75d
moc25400 <user08@user08de-imac.local>
2019-03-06 14:26:58 +0800

    Initial Commit

---
+++ a/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  a
+//
+//  Created by User08 on 2019/3/6.
+//  Copyright © 2019 user08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ a/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  a
+//
+//  Created by User08 on 2019/3/6.
+//  Copyright © 2019 user08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
