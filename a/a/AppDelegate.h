cfdcb1cdfe31c09c2cbf7320b5f5186668ac1726
shouchungchang <43200455+shouchungchang@users.noreply.github.com>
2019-03-08 12:54:02 +0800

    Add files via upload

---
+++ a/a/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  a
+//
+//  Created by User08 on 2019/3/6.
+//  Copyright © 2019 user08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
